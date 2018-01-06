11c281a96f6e3366e4c4baaed28aee2f332205b1
santiago-salas-v <santiago.salas.v@hotmail.de>
2018-01-06 19:17:32 +0100

    Validierungen ergaenzt

--- bsp_pat_ue_03_2.py
+++ bsp_pat_ue_03_2.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import integrate
 import z_l_v
 
 # Modell feststellen
@@ -12,80 +13,62 @@ r = 8.314  # J/(mol K)
 
 namen = ['CO', 'H2', 'CO2', 'H2O', 'CH4', 'NH3', 'AR', 'O2', 'N2']
 ne = np.array([
-0, 0, 0, 60000, 20000, 0, 
-0.01*15000,
-0.21*15000,
-0.78*15000
-], dtype=float) # kmol/h
+    0, 0, 0, 60000, 20000, 0,
+    0.01 * 15000,
+    0.21 * 15000,
+    0.78 * 15000
+], dtype=float)  # kmol/h
 
 nuij = np.array([
     [-1, +1, +1, -1, +0, +0, +0, +0, +0],
     [-1, -3, +0, +1, +1, +0, +0, +0, +0],
     [+0, -4, -1, +2, +1, +0, +0, +0, +0],
-    [+0, -3/2, 0, 0, +0, +1, +0, +0, -1/2]
-    ]).T
+    [+0, -3 / 2, 0, 0, +0, +1, +0, +0, -1 / 2]
+]).T
 
 # Thermochemische Daten
 
-# Barin, Ihsan: Thermochemical Data of Pure Substances. 
+# Barin, Ihsan: Thermochemical Data of Pure Substances.
 # Weinheim, New York: VCH, 1993.
 
 h_298 = np.array(
-        [-110.541, 0., -393.505, 
-        -241.826, -74.873, -45.940,
-        0., 0., 0.]) * 1000  # J/mol
+    [-110.541, 0., -393.505,
+     -241.826, -74.873, -45.940,
+     0., 0., 0.]) * 1000  # J/mol
 
 g_298 = np.array(
     [-169.474, -38.962, -457.240,
-    -298.164, -130.393, -103.417,
-    -46.167, -61.165, -57.128
-    ]) * 1000  # J/mol 
+     -298.164, -130.393, -103.417,
+     -46.167, -61.165, -57.128
+     ]) * 1000  # J/mol
 
 # Kritische Parameter Tc, Pc, omega(azentrischer Faktor)
 
 # e.V., VDI: VDI-Wärmeatlas. Wiesbaden: Springer Berlin Heidelberg, 2013.
 
 tc = np.array([
-    132.86, 33.19, 304.13, 
-    647.10, 190.56, 405.50, 
+    132.86, 33.19, 304.13,
+    647.10, 190.56, 405.50,
     150.69, 154.60, 126.19
-    ])  # K
+])  # K
 
 pc = np.array([
     34.98, 13.15, 73.77,
     220.64, 45.99, 113.59,
     48.63, 50.46, 33.96
-    ])  # bar
+])  # bar
 
 omega_af = np.array([
     0.050, -0.219, 0.224,
     0.344, 0.011, 0.256,
     -0.002, 0.022, 0.037
-    ])
-
+])
+# umformen (reshape), um direkte Division zu ermöglichen
 mm = np.array([
-    28.01, 2.02, 44.01, 
-    18.02, 16.04, 17.03, 
+    28.01, 2.02, 44.01,
+    18.02, 16.04, 17.03,
     39.95, 32.00, 28.01
-    ]).reshape([len(namen),1])
-
-# Koeffizienten für Cp(T)/R = A + B*T + C*T^2 + D*T^-2, T[=]K
-# Nach rechts hin: A, B, C, D
-
-# Smith, J.M.; Van Ness, Hendrick ; Abbott, Michael 
-# Introduction to Chemical Engineering Thermodynamics. 
-# New York: McGraw-Hill Education, 2017.
-cp_coefs = np.array([z for z in [
-    [
-        y.replace(',', '.') for y in x.split('  ')
-    ] for x in """
-3,3760E+00  5,5700E-04  0,0000E+00  -3,1000E+03
-3,2490E+00  4,2200E-04  0,0000E+00  8,3000E+03
-5,4570E+00  1,0450E-03  0,0000E+00  -1,1570E+05
-3,4700E+00  1,4500E-03  0,0000E+00  1,2100E+04
-1,7020E+00  9,0810E-03  -2,1640E-06  0,0000E+00
-3,5780E+00  3,0200E-03  0,0000E+00  -1,8600E+04
-""".split('\n') if len(x) > 0] if len(z) > 1], dtype=float)
+]).reshape([len(namen), 1])
 
 # Koeffizienten für Cp(T)/R = B+(C-B)(T/(A+T))^2*(
 # 1-A/(A+T)*(D+E*T/(A+T)+F*(T/(A+T))^2+G*(T/(A+T))^3))
@@ -95,7 +78,7 @@ cp_coefs = np.array([z for z in [
 
 cp_coefs = np.array([z for z in [
     [
-        y.replace(',', '.').replace('–','-') for y in x.split('  ')
+        y.replace(',', '.').replace('–', '-') for y in x.split('  ')
     ] for x in """
 407,9796  3,5028  2,8524  –2,3018  32,9055  –100,1815  106,1141
 392,8422  2,4906  –3,6262  –1,9624  35,6197  –81,3691  62,6668
@@ -108,47 +91,62 @@ cp_coefs = np.array([z for z in [
 432,2027  3,5160  2,8021  –4,1924  42,0153  –114,2500  111,1019
 """.split('\n') if len(x) > 0] if len(z) > 1], dtype=float)
 
-def cp_durch_r(t):
-    a,b,c,d,e,f,g = np.split(cp_coefs,cp_coefs.shape[1], axis=1)
-    return b+(c-b)*(t/(a+t))**2*(
-    1-a/(a+t)*(
-    d+e*t/(a+t)+f*(t/(a+t))**2+g*(t/(a+t))**3
-    )) # dimensionslos
 
-print('298,15°K')
-print(cp_durch_r(298.15)*8.314/mm)
-print('773,15°K')
-print(cp_durch_r(500+273.15)*8.314/mm)
+def cp_durch_r(t, component=-1):
+    if component != -1:
+        cp_c_temp = cp_coefs[component, :]
+        a, b, c, d, e, f, g = np.split(cp_c_temp, len(cp_c_temp), axis=0)
+    else:
+        a, b, c, d, e, f, g = np.split(cp_coefs, cp_coefs.shape[1], axis=1)
+    return b + (c - b) * (t / (a + t))**2 * (
+        1 - a / (a + t) * (
+            d + e * t / (a + t) + f * (t / (a + t))**2 + g * (t / (a + t))**3
+        ))  # dimensionslos
 
 # Berechne H(T), G(T) und K(T) mit Cp(T)
 
+
 def h(t):
-    return (
-        h_298 +
-        r * cp_coefs[:, 0] * (t - t0_ref) +
-        r * cp_coefs[:, 1] / 2. * (t ** 2 - t0_ref ** 2) +
-        r * cp_coefs[:, 2] / 3. * (t ** 3 - t0_ref ** 3) -
-        r * cp_coefs[:, 3] * (1 / t - 1 / t0_ref)
-    )  # J/mol
+    enthalpien = np.empty_like(h_298)
+    for i in range(len(enthalpien)):
+        int_cp_durch_r = integrate.quad(
+            lambda temp: cp_durch_r(temp, i), 298.15, t)[0]
+        enthalpien[i] = h_298[i] + r * int_cp_durch_r
+    return enthalpien  # J/mol
+
 
 def g(t, h_t):
-    return (
-        h_t - t / t0_ref * (h_298 - g_298) -
-        r * cp_coefs[:, 0] * t * np.log(t / t0_ref) -
-        r * cp_coefs[:, 1] * t ** 2 * (1 - t0_ref / t) -
-        r * cp_coefs[:, 2] / 2. * t ** 3 * (1 - (t0_ref / t) ** 2) +
-        r * cp_coefs[:, 3] / 2. * 1 / t * (1 - (t / t0_ref) ** 2)
-    )  # J/mol
+    freie_energien = np.empty_like(h_298)
+    for i in range(len(freie_energien)):
+        int_cp_durch_rt = integrate.quad(
+            lambda temp: cp_durch_r(temp, i) / temp, 298.15, t)[0]
+        freie_energien[i] = \
+            h_t[i] - \
+            t / t0_ref * (h_298[i] - g_298[i]) - r * t * int_cp_durch_rt
+    return freie_energien  # J/mol
+
 
 def k(t, g_t):
     delta_g_t = nuij.T.dot(g_t)
     return np.exp(-delta_g_t / (r * t))
 
+
 delta_gr_298 = nuij.T.dot(g_298)
 
 delta_hr_298 = nuij.T.dot(h_298)
 
-cp_493 = cp(493.15)  # J/(mol K)
-h_493 = h(493.15)  # J/mol
+cp_493 = r * cp_durch_r(493.15)  # J/(mol K)
+h_ein = h(323.09 + 273.15)  # J/mol
+print(h(298.15))
+print(g(298.15, h(298.15)))
+print(np.sum(h_ein * ne / sum(ne)) * 1000)
+h_493 = h(493.15)
 g_493 = g(493.15, h_493)  # J/mol
+print(g_493)
 k_493 = k(493.15, g_493)  # []
+print(nuij.T.dot(g_493))
+print(k(127+273.15, g(127+273.15, h(127+273.15)))[-1]*(101325.0/(8.314*1000*(127+273.16)))**(1-1/2-3/2)) # Als Konz.
+print(k(25+273.15, g(25+273.15, h(25+273.15)))[-1]) # [762] S. 158 Bsp. 13.2: Weingärtner, Hermann: Chemische Thermodynamik : Einführung für Chemiker und Chemieingenieure. Berlin Heidelberg New York: Springer-Verlag, 2013.
+print(k(1000, g(1000, h(1000)))[-1]) # [5,45E-04] S. 160 Bsp. 13.3: Weingärtner, Hermann: Chemische Thermodynamik : Einführung für Chemiker und Chemieingenieure. Berlin Heidelberg New York: Springer-Verlag, 2013.
+print(k(1100, g(1100, h(1100)))[0]) # [1,0] S. 502 Ex. 13.5: SVN
+print(k(273.15+450, g(273.15+450, h(273.15+450)))[-1]) # [6,59E-03] S. 52: Baerns, Manfred ; Behr, Arno ; Brehm, Axel ; Gmehling, JÃ1⁄4rgen ; Hinrichsen, Kai-Olaf ; Hofmann, Hanns ; Onken, Ulfert ; Palkovits, Regina ; Renken, Albert: Technische Chemie. New York: John Wiley & Sons, 2014.
