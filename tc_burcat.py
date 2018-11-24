fd2183a0563d7aa3cdc79f85a3591af4bca6aa17
santiago-salas-v <santiago.salas.v@hotmail.de>
2018-11-24 21:10:15 +0100

    table joins burcat and ant.-coefficients

--- tc_burcat.py
+++ tc_burcat.py
@@ -1,5 +1,6 @@
-import os, sys
+import os, sys, re
 import locale
+from numpy import loadtxt
 from functools import partial
 import lxml.etree as ET
 import csv, io
@@ -7,12 +8,13 @@ from PyQt5.QtWidgets import QTableView, QApplication, QWidget
 from PyQt5.QtWidgets import QDesktopWidget, QGridLayout, QLineEdit
 from PyQt5.QtWidgets import QComboBox, QLabel, QTableWidget, QTableWidgetItem
 from PyQt5.QtCore import QAbstractTableModel, QModelIndex, Qt, QVariant, QEvent
-from PyQt5.QtGui import QKeySequence, QGuiApplication
+from PyQt5.QtGui import QKeySequence, QGuiApplication, QFont
 from PyQt5.QtCore import pyqtSignal as SIGNAL
 
 
 locale.setlocale(locale.LC_ALL, '')
-source_xml_file = './data/BURCAT_THR.xml'
+burcat_xml_file = './data/BURCAT_THR.xml'
+yaws_csv_file = './data/The-Yaws-Handbook-of-Vapor-Pressure-Second-Edition-Antoine-coefficients_2.csv'
 
 class App(QWidget):
     def __init__(self):
@@ -20,8 +22,8 @@ class App(QWidget):
         self.title = 'Thermodynamic data from burcat'
 
         screen = QDesktopWidget().screenGeometry()
-        width = 300
-        height = 200
+        width = 600
+        height = 300
 
         x = screen.width()/2 - width/2
         y = screen.height()/2 - height/2
@@ -45,25 +47,28 @@ class App(QWidget):
         self.phase_filter = QComboBox()
 
         self.tableView1.setModel(self.model)
+        self.tableWidget1.added_column_names = ['']
         self.tableWidget1.setColumnCount(self.tableView1.model().columnCount())
-        self.tableWidget1.setHorizontalHeaderLabels(self.tableView1.model().column_names)
+        self.tableWidget1.setHorizontalHeaderLabels(self.tableView1.model().column_names_burcat)
         self.phase_filter.addItems(['', 'G', 'L', 'S', 'C'])
-        self.tableWidget1.setVisible(False)
+        self.tableWidget1.setEnabled(False)
         self.tableWidget1.setSelectionBehavior(QTableWidget.SelectRows)
         self.tableWidget1.installEventFilter(self)
 
         # Add box layout, add table to box layout and add box layout to widget
         self.layout = QGridLayout()
         self.layout.addWidget(self.tableView1, 1, 1, 1, 4)
-        self.layout.addWidget(self.cas_filter, 2, 1, 1, 1)
-        self.layout.addWidget(self.name_filter, 2, 2, 1, 1)
-        self.layout.addWidget(self.formula_filter, 2, 3, 1, 1)
-        self.layout.addWidget(self.phase_filter, 2, 4, 1, 1)
-        self.layout.addWidget(QLabel('cas'), 3, 1, 1, 1)
-        self.layout.addWidget(QLabel('name'), 3, 2, 1, 1)
-        self.layout.addWidget(QLabel('formula'), 3, 3, 1, 1)
-        self.layout.addWidget(QLabel('phase'), 3, 4, 1, 1)
-        self.layout.addWidget(self.tableWidget1, 4, 1, 1, 4)
+        self.layout.addWidget(self.cas_filter, 3, 1, 1, 1)
+        self.layout.addWidget(self.name_filter, 3, 2, 1, 1)
+        self.layout.addWidget(self.formula_filter, 3, 3, 1, 1)
+        self.layout.addWidget(self.phase_filter, 3, 4, 1, 1)
+        self.layout.addWidget(QLabel('find by: cas'), 2, 1, 1, 1)
+        self.layout.addWidget(QLabel('name'), 2, 2, 1, 1)
+        self.layout.addWidget(QLabel('formula'), 2, 3, 1, 1)
+        self.layout.addWidget(QLabel('phase'), 2, 4, 1, 1)
+        self.layout.addWidget(QLabel('selection'), 4, 1, 1, 4)
+        self.layout.addWidget(self.tableWidget1, 5, 1, 1, 4)
+
 
 
         self.setLayout(self.layout)
@@ -78,9 +83,6 @@ class App(QWidget):
         self.tableView1.selectionModel().selectionChanged.connect(partial(
             self.add_selection_to_widget
         ))
-        self.tableWidget1.cellDoubleClicked.connect(partial(
-            self.delete_selected_row
-        ))
 
         # Show widget
         self.show()
@@ -88,8 +90,8 @@ class App(QWidget):
     def add_selection_to_widget(self):
         indexes = self.tableView1.selectedIndexes()
         index = indexes[0]
-        column_of_cas = self.tableView1.model().column_names.index('cas')
-        column_of_phase = self.tableView1.model().column_names.index('phase')
+        column_of_cas = self.tableView1.model().column_names_burcat.index('cas')
+        column_of_phase = self.tableView1.model().column_names_burcat.index('phase')
         phase = self.tableView1.model().index(index.row(), column_of_phase).data()
         cas = self.tableView1.model().index(index.row(), column_of_cas).data()
         already_in_table = False
@@ -98,23 +100,24 @@ class App(QWidget):
                 already_in_table = True
         if not already_in_table:
             self.tableWidget1.setRowCount(self.tableWidget1.rowCount() + 1)
-            for column_name in self.tableView1.model().column_names:
-                column_no = self.tableView1.model().column_names.index(column_name)
+            for column_name in self.tableView1.model().column_names_burcat:
+                column_no = self.tableView1.model().column_names_burcat.index(column_name)
                 data = self.tableView1.model().index(index.row(), column_no).data()
                 if type(data) != str:
                     item_to_add = QTableWidgetItem(locale.str(data))
                 else:
                     item_to_add = QTableWidgetItem(data)
-                item_to_add.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)
+                item_to_add.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled| Qt.ItemIsEditable)
                 self.tableWidget1.setItem(
                     self.tableWidget1.rowCount()-1, column_no,
                     item_to_add)
+            column_no += 1
         if len(indexes) > 0 or self.tableWidget1.rowCount() > 0:
-            self.tableWidget1.setVisible(True)
+            self.tableWidget1.setEnabled(True)
 
 
     def copy_selection(self):
-        col_names = self.tableView1.model().column_names
+        col_names = self.tableView1.model().column_names_burcat
         selection = self.tableWidget1.selectedIndexes()
         if selection:
             rows = sorted(index.row() for index in selection)
@@ -136,24 +139,59 @@ class App(QWidget):
                 event.matches(QKeySequence.Copy)):
             self.copy_selection()
             return True
+        if (event.type() == QEvent.KeyPress and
+                event.matches(QKeySequence.Delete)):
+            if len(self.tableWidget1.selectedIndexes()) > 1:
+                self.delete_selected()
+            return True
         return super(App, self).eventFilter(source, event)
 
-    def delete_selected_row(self):
-        index = self.tableWidget1.selectedIndexes()[0]
-        self.tableWidget1.removeRow(index.row())
+    def delete_selected(self):
+        while len(self.tableWidget1.selectedIndexes()) > 0:
+            self.tableWidget1.removeRow(self.tableWidget1.selectedIndexes()[0].row())
+        if self.tableWidget1.rowCount() == 0:
+            self.tableWidget1.setEnabled(False)
 
+
+def helper_func1(x):
+    # helper function for csv reading
+    str_with_dot_actually_minus = x
+    matches = re.search(b'(\.(\d+\.\d+))', x)
+    if matches:
+        str_with_dot_actually_minus = b'-' + matches.groups()[1]
+    return str_with_dot_actually_minus.replace(b' ', b'')
 
 
 class thTableModel(QAbstractTableModel):
 
     def __init__(self):
         super(thTableModel, self).__init__()
-        self.tree = ET.parse(source_xml_file)
+        self.tree = ET.parse(burcat_xml_file)
         self.root = self.tree.getroot()
         self.cas_filter = ''
         self.name_filter = ''
         self.formula_filter = ''
         self.phase_filter = ''
+
+        self.ant = loadtxt(
+            open(yaws_csv_file, 'rb'),
+            delimiter='|',
+            skiprows=2,
+            dtype={
+                'names': [
+                    'ant_no', 'ant_formula', 'ant_name',
+                    'ant_cas', 'ant_a', 'ant_b',
+                    'ant_c', 'ant_tmin', 'ant_tmax',
+                    'ant_code'],
+                'formats': [
+                    int, object, object,
+                    object, float, float,
+                    float, float, float, object]},
+            converters={
+                0: helper_func1, 4: helper_func1,
+                5: helper_func1, 6: helper_func1,
+                7: helper_func1
+            })
         #self.xpath = \
         #    ".//formula_name_structure[contains(formula_name_structure_1, '" + \
         #    self.name_filter + "')]/" + "../../specie[contains(@CAS, '')]/" +\
@@ -168,7 +206,7 @@ class thTableModel(QAbstractTableModel):
             "phase[contains(phase, '" + \
             self.phase_filter + "')]"
         self.results = self.root.xpath(self.xpath)
-        self.column_names = [
+        self.column_names_burcat = [
             'cas', 'formula_name_structure_1',
             'phase', 'formula', 'molecular_weight',
             'hf298_div_r', 'range_tmin_to_1000',
@@ -176,6 +214,12 @@ class thTableModel(QAbstractTableModel):
             'range_1000_to_tmax',
             'a1_high', 'a2_high', 'a3_high', 'a4_high',
             'a5_high', 'a6_high', 'a7_high']
+        self.column_names_ant = [
+            'ant_no', 'ant_formula', 'ant_name',
+            'ant_cas', 'ant_a', 'ant_b',
+            'ant_c', 'ant_tmin', 'ant_tmax',
+            'ant_code'
+        ]
 
     def apply_filter(self, cas_filter, name_filter, formula_filter, phase_filter):
         self.name_filter = name_filter.text().upper()
@@ -246,13 +290,32 @@ class thTableModel(QAbstractTableModel):
                 a6_high = float(coef.text.replace(' ', ''))
             if coef.get('name') == 'a7':
                 a7_high = float(coef.text.replace(' ', ''))
+
+        ant_record = self.ant[self.ant['ant_cas'] == cas]
+        ant_no, ant_formula, ant_name, \
+        ant_cas, ant_a, ant_b, ant_c, \
+        ant_tmin, ant_tmax, ant_code = [None for i in range(len(self.column_names_ant))]
+        if len(ant_record) == 1:
+            ant_no, ant_formula, ant_name, \
+            ant_cas,  ant_a, ant_b, ant_c, \
+            ant_tmin, ant_tmax, ant_code = ant_record[0]
+            ant_no = int(ant_no)
+            ant_a = float(ant_a)
+            ant_b = float(ant_b)
+            ant_c = float(ant_c)
+            ant_tmin = float(ant_tmin)
+            ant_tmax = float(ant_tmax)
+
         if not index.isValid() or \
                 index.row() < 0 or index.row() > len(self.results) or \
                 index.column() < 0 or index.column() > self.columnCount():
             return QVariant()
         column = index.column()
         if role == Qt.DisplayRole:
-            return QVariant(locals()[self.column_names[column]])
+            if column < len(self.column_names_burcat):
+                return QVariant(locals()[self.column_names_burcat[column]])
+            else:
+                return QVariant(locals()[self.column_names_ant[column-len(self.column_names_burcat)]])
 
     def headerData(self, section: int, orientation: Qt.Orientation, role: int= Qt.DisplayRole):
         if role == Qt.TextAlignmentRole:
@@ -262,7 +325,10 @@ class thTableModel(QAbstractTableModel):
         if role != Qt.DisplayRole:
             return QVariant()
         if orientation == Qt.Horizontal:
-            return QVariant(self.column_names[section])
+            if section < len(self.column_names_burcat):
+                return QVariant(self.column_names_burcat[section])
+            else:
+                return QVariant(self.column_names_ant[section-len(self.column_names_burcat)])
         if orientation == Qt.Vertical:
             return QVariant(section + 1)
 
@@ -282,10 +348,11 @@ class thTableModel(QAbstractTableModel):
         # (phase)coefficients range_Tmin_to_1000 a6,
         # (phase)coefficients range_Tmin_to_1000 a7,
         # (phase)coefficients hf298_div_r
-        return len(self.column_names)
+        return len(self.column_names_burcat) + len(self.column_names_ant)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    QApplication.setFont(QFont('Consolas', 10))
     ex = App()
     sys.exit(app.exec_())
