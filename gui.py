b62dc8113ada4d05e331f59972ee1c830c90f421
santiago-salas-v <santiago.salas.v@hotmail.de>
2023-11-13 01:58:31 +0000

    streamlit caching reduces load times

--- gui.py
+++ gui.py
@@ -1,51 +1,58 @@
+import streamlit
 from streamlit import write
 from lxml import etree
 from os.path import sep
 from pandas import DataFrame
 import string
 
-data = sep.join(['data', 'BURCAT_THR.xml'])
-template = sep.join(['data', 'xsl_stylesheet_burcat.xsl'])
+data_path = sep.join(['data', 'BURCAT_THR.xml'])
+template_path = sep.join(['data', 'xsl_stylesheet_burcat.xsl'])
 
-tree = etree.parse(data)
-root = tree.getroot()
-xsl = etree.parse(template)
-transformer = etree.XSLT(xsl)
-result = transformer(tree)
+@streamlit.cache_data
+def load_data():
+    tree = etree.parse(data_path)
+    root = tree.getroot()
+    xsl = etree.parse(template_path)
+    transformer = etree.XSLT(xsl)
+    result = transformer(tree)
 
-cas_filter, name_filter, \
-formula_filter, phase_filter = \
-'', '', '', ''
-
-
-xpath = \
-            "./specie[contains(@CAS, '" + \
-            cas_filter + "')]/" + \
-            "formula_name_structure[contains(translate(" +\
-            "formula_name_structure_1" + \
-            ", '" + string.ascii_lowercase + "', '" + \
-            string.ascii_uppercase + "'), '" + \
-            name_filter + "')]/../@CAS/../" + \
-            "phase[contains(translate(" + \
-            "formula" + \
-            ", '" + string.ascii_lowercase + "', '" + \
-            string.ascii_uppercase + "'), '" + \
-            formula_filter + "')]/../" + \
-            "phase[contains(phase, '" + \
-            phase_filter + "')]"
+    cas_filter, name_filter, \
+    formula_filter, phase_filter = \
+    '', '', '', ''
 
 
-for element in root.xpath(xpath):
-    #print(element.getparent().get('CAS'))
-    pass
+    xpath = \
+                "./specie[contains(@CAS, '" + \
+                cas_filter + "')]/" + \
+                "formula_name_structure[contains(translate(" +\
+                "formula_name_structure_1" + \
+                ", '" + string.ascii_lowercase + "', '" + \
+                string.ascii_uppercase + "'), '" + \
+                name_filter + "')]/../@CAS/../" + \
+                "phase[contains(translate(" + \
+                "formula" + \
+                ", '" + string.ascii_lowercase + "', '" + \
+                string.ascii_uppercase + "'), '" + \
+                formula_filter + "')]/../" + \
+                "phase[contains(phase, '" + \
+                phase_filter + "')]"
 
-data = []
-for i in result.xpath('/*'):
-    inner = {}
-    for j in i.xpath('*'):
-        inner[j.tag] = j.text
-    data.append(inner)
 
-burcat_df = DataFrame(data)
+    for element in root.xpath(xpath):
+        #print(element.getparent().get('CAS'))
+        pass
 
-write(burcat_df)
+    data = []
+    for i in result.xpath('/*'):
+        inner = {}
+        for j in i.xpath('*'):
+            inner[j.tag] = j.text
+        data.append(inner)
+
+
+    burcat_df = DataFrame(data)
+    return burcat_df
+
+df = load_data()
+write(df)
+
